47e521d7183d249ea5503621338a612001b8a7fd
muhammadfarros12 <muhammadfarroos12@gmail.com>
2021-03-31 15:48:40 +0700

    Profile Tutorial Finished

--- app/src/main/java/com/farroos/foodmarket/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/farroos/foodmarket/ui/profile/ProfileFragment.kt
@@ -5,15 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.farroos.foodmarket.FoodMarket
 import com.farroos.foodmarket.R
+import com.farroos.foodmarket.model.response.login.User
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
         return root
@@ -23,10 +27,22 @@ class ProfileFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         val sectionPagerAdapter = SectionPagerAdapter(
-            childFragmentManager
+                childFragmentManager
         )
         view_pager.adapter = sectionPagerAdapter
         tab_layout.setupWithViewPager(view_pager)
+
+        var user = FoodMarket.getApp().getUser()
+        var userResponse = Gson().fromJson(user, User::class.java)
+
+        txt_name.setText(userResponse.name)
+        txt_email.setText(userResponse.email)
+
+        if (!userResponse.profilePhotoUrl.isNullOrEmpty()) {
+            Glide.with(requireActivity())
+                    .load(userResponse.profilePhotoUrl)
+                    .into(img_picture)
+        }
 
     }
 
